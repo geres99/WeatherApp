a97cb9d87130b46d5509a83e68efcab1296cbd07
Marcin Medryk <medryk.kontakt@gmail.com>
2020-10-17 19:24:08 +0200

    3rd

--- scripts.js
+++ scripts.js
@@ -8,14 +8,20 @@ async function fetchData() {
 }
     const citiesList = JSON.parse(cities)
     console.log(citiesList)
+    let v = 0
     for(let i = 0; i < citiesList.length; i++) {
         let countrys = citiesList[i].country
         let div = document.createElement("div")
         div.className = "country" + i
         let rozmowa = document.createTextNode(countrys)
         div.appendChild(rozmowa)
-        if(!(rozmowa.nodeValue === "Poland")) {
+        if (i > 0) {
+        if(!(rozmowa.nodeValue === document.querySelector(".country" + v).innerHTML)) {
         document.body.appendChild(div)
+        v = i
         }
         }
-        console.log(countrys)
+        else {
+            document.body.appendChild(div) 
+        }
+    }
