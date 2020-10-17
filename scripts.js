c05304b8a19f1cef67699fe27d33ce8e9faf3221
Marcin Medryk <medryk.kontakt@gmail.com>
2020-10-17 23:28:44 +0200

    Commit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 async function fetchData() {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=warsaw&appid=d3c8097a4728795abb4a589545a952ee");
-    //console.log("dupa17", response);
     const data = await response.text();
-    //console.log("DATA:", data);
     const obj = JSON.parse(data)
     document.getElementById("testingObj").innerHTML = obj.name
+
 }
     const citiesList = JSON.parse(cities)
     console.log(citiesList)
@@ -17,26 +16,31 @@ async function fetchData() {
         if (!(map.has(TextNodeCities.nodeValue))) {
         map.set(TextNodeCities.nodeValue)
         document.getElementById("selectorOfCountry").appendChild(div) 
-        }
-        let div1 = document.createElement("option")
-        let citiesOfCountry = citiesList[i].name
-        let TextNodeCitiesOfCountry = document.createTextNode(citiesOfCountry)
-        div1.appendChild(TextNodeCitiesOfCountry)
-        div1.className = citiesList[i].country
-        div1.className = "hiddenCountry"
-        document.getElementById("selectorOfCity").appendChild(div1)
-
+        }   
+        let div2 = document.createElement("option")
+        let citiesOfWorld = citiesList[i].name
+        let TextNodeCitiesOfWorld = document.createTextNode(citiesOfWorld)
+        div2.appendChild(TextNodeCitiesOfWorld)
+        div2.className = citiesList[i].country
+        document.getElementById("selectorOfCity").appendChild(div2) 
     }
+    document.body.addEventListener("mousedown", function() {
+        if(!(document.querySelector("#selectorOfCountry").value === "Select your country")) {
+            let div3 = document.querySelector(".hidden")
+            div3.style.display = "initial"
+        }
+    })
     let EventListenerKillerCountry = function() {
         if(!(document.querySelector("#selectorOfCountry").value === "Select your country")) {
         let deletingOption = document.querySelector("#deletedOption")
         deletingOption.remove()
         document.body.removeEventListener("mousedown", EventListenerKillerCountry)
-
+        
         let divOption = document.createElement("option")
         let divTextNode = document.createTextNode("Select your city")
         divOption.appendChild(divTextNode)
         document.querySelector("#selectorOfCity").appendChild(divOption)
-        }
+    }
     }
     document.body.addEventListener("mousedown", EventListenerKillerCountry)
+console.log(selectorOfCountry[1])
