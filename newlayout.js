82d8a5afb2b80d50ab93a69adcec641e1be72b96
Marcin Medryk <medryk.kontakt@gmail.com>
2020-10-20 14:01:00 +0200

    improved UI

--- newlayout.js
+++ newlayout.js
@@ -25,18 +25,18 @@ for(let i = 0; i < countryListArray.length; i++) {
 
 document.querySelector("#selectorOfCountry").addEventListener("change", function() {
     if(!(document.querySelector("#selectorOfCountry").value === "Select your country")) {
-        let dupa = document.createElement("img")
-        dupa.src = "Images/Flags/" + document.querySelector("#selectorOfCountry").value + "-flag-small.png"
-        dupa.style = "width:25px"
-        dupa.className = "flex-item"
-        let dupa1 = document.createElement("img")
-        dupa1.src = "Images/Flags/" + document.querySelector("#selectorOfCountry").value + "-flag-small.png"
-        dupa1.style = "width:25px"
-        dupa1.className = "flex-item"
+        let countryFlagChosen = document.createElement("img")
+        countryFlagChosen.src = "Images/Flags/" + document.querySelector("#selectorOfCountry").value + "-flag-small.png"
+        countryFlagChosen.style = "width:25px"
+        countryFlagChosen.className = "flex-item"
+        let CityImageChosen = document.createElement("img")
+        CityImageChosen.src = "Images/Flags/" + document.querySelector("#selectorOfCountry").value + "-flag-small.png"
+        CityImageChosen.style = "width:25px"
+        CityImageChosen.className = "flex-item"
         document.querySelector("#countryFlag").innerHTML = ""
         document.querySelector("#cityImage").innerHTML = ""
-        document.querySelector("#countryFlag").appendChild(dupa)
-        document.querySelector("#cityImage").appendChild(dupa1)
+        document.querySelector("#countryFlag").appendChild(countryFlagChosen)
+        document.querySelector("#cityImage").appendChild(CityImageChosen)
         document.querySelector("#selectorOfCity").innerHTML = ""
         for(let i = 0; i < map.get(document.querySelector("#selectorOfCountry").value).length - 1; i++) {
                 let div2 = document.createElement("option")
@@ -62,5 +62,28 @@ async function fetchData() {
     const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + document.querySelector("#selectorOfCity").value + "&appid=d3c8097a4728795abb4a589545a952ee");
     const data = await response.text();
     const obj = JSON.parse(data)
-    document.querySelector(".temp").innerHTML = "The weather in " + obj.name + " is " + obj.weather[0].main + " with temperature of " + (Math.floor(obj.main.temp - 273.15)) + "C"
+    let WeatherImage = document.createElement("img")
+        WeatherImage.src = "Images/Weather/3127120-weather/png/" + obj.weather[0].main + ".png"
+        WeatherImage.style = "width:200px"
+        document.querySelector(".weather").innerHTML = ""
+        document.querySelector(".weather").appendChild(WeatherImage)
+    let div = document.createElement("div")
+    div.className = "textTemperature"
+    let temperature = document.createTextNode("TEMPERATURE: " + (Math.floor(obj.main.temp - 273.15)) + "C")
+    div.appendChild(temperature)
+
+    let TempImage = document.createElement("img")
+    if((Math.floor(obj.main.temp - 273.15)) <= 5) {
+        TempImage.src = "Images/Weather/3127120-weather/png/" + "TempLow" + ".png"
+    }
+    if((Math.floor(obj.main.temp - 273.15) > 5 && Math.floor(obj.main.temp - 273.15) <= 25)) {
+        TempImage.src = "Images/Weather/3127120-weather/png/" + "TempMedium" + ".png"
+    }
+    if((Math.floor(obj.main.temp - 273.15) > 25)) {
+        TempImage.src = "Images/Weather/3127120-weather/png/" + "TempHigh" + ".png"
+    }
+    TempImage.style = "width:18px"
+    TempImage.style = "height:18px"
+    div.appendChild(TempImage)
+    document.querySelector(".weather").appendChild(div)
 }
